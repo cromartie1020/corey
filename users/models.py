f0b82094640c4e4658802eb9042756ba3d56428d
cromartie1020 <hrc245@gmail.com>
2021-08-27 21:29:47 -0400

    unique email address in models

--- users/models.py
+++ users/models.py
@@ -1,13 +1,14 @@
 from django.db import models
-from django.contrib.auth import get_user_model
 from PIL import Image
-User = get_user_model()
+from django.contrib.auth.models import User
+# How to make a email field unique.
+User._meta.get_field('email')._unique = True
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile-pics')
-    #email = models.EmailField(blank=True, null=True)
+    email = models.EmailField(unique=True, blank=True, null=True)
 
     def __str__(self):
         return f'{self.user.username} Profile'
